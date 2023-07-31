85ad7372d2ea8abeee486a573f4d7848bc696c32
nayab1711 <42082594+nayab1711@users.noreply.github.com>
2023-07-31 16:07:46 +0530

    Add files via upload

---
+++ 3-tier/simpleapp-terraform/storageaccount.tf
@@ -0,0 +1,11 @@
+resource "azurerm_storage_account" "Demo-storage" {
+  name                     = "fgstorageaccount1989"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
